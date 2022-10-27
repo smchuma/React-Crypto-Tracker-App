73619f1619dae54404dab5f9e31376208233c77d
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-10-27 16:37:58 +0300

    testing continous deployment, added a h1 tag

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import { Routes, Route} from 'react-router-dom'
-import Header from './components/Header'
-import './App.css'
-import CoinPage from './pages/CoinPage'
-import Homepage from './pages/Homepage'
-import { makeStyles } from '@material-ui/styles'
-import Exchanges from './pages/Exchanges'
-import Footer from './components/Footer'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import "./App.css";
+import CoinPage from "./pages/CoinPage";
+import Homepage from "./pages/Homepage";
+import { makeStyles } from "@material-ui/styles";
+import Exchanges from "./pages/Exchanges";
+import Footer from "./components/Footer";
 
-
-const useStyles = makeStyles(() =>({
-    App: {
-        backgroundColor: "#011140",
-        color: "white",
-        minHeight: "100vh",
-    },
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#011140",
+    color: "white",
+    minHeight: "100vh",
+  },
 }));
 
 const App = () => {
-
-    const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <div className={classes.App}>
-        <Header />
-        <Routes>
+      <h1>Try to see if continuos deployment works</h1>
+      <Header />
+      <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="/coins/:id" element={<CoinPage />} />
         <Route exact path="/exchanges" element={<Exchanges />} />
-        
-        </Routes>
-        <div className="footer">
+      </Routes>
+      <div className="footer">
         <Footer />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
